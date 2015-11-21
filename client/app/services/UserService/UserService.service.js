a024e44c8898040c5c0c75b642be1ccad2d466c8
johnellis1392 <john.ellis1392@gmail.com>
2015-11-21 11:26:31 -0800

    Update UserService with Restangular routes

--- client/app/services/UserService/UserService.service.js
+++ client/app/services/UserService/UserService.service.js
@@ -27,6 +27,7 @@ angular.module('seniorprojectYoApp')
             }
         });
 
+        var Users = Restangular.service(Constants.routes.users);
 
 
         this.index = function(_params, callback, error) {
@@ -35,30 +36,22 @@ angular.module('seniorprojectYoApp')
                 offset: _params.offset || 0
             };
 
-            Restangular.one(Constants.routes.users)
-                .get(params)
-                .then(callback, error);
+            Users.getList().then(callback, error);
         };
 
 
         this.show = function(id, callback, error) {
-            Restangular.one(Constants.routes.users, id)
-                .get()
-                .then(callback, error);
+            Users.one(id).get().then(callback, error);
         };
 
 
         this.update = function(id, params, callback, error) {
-            Restangular.one(Constants.routes.users, id)
-                .customPUT({user: params})
-                .then(callback, error);
+            Users.one(id).patch({user: params}).then(callback, error);
         };
 
 
         this.destroy = function(id, callback, error) {
-            Restangular.one(Constants.routes.users, id)
-                .remove()
-                .then(callback, error);
+            Users.one(id).remove().then(callback, error);
         };
 
 
@@ -69,49 +62,15 @@ angular.module('seniorprojectYoApp')
 
 
             /**
-             * Create an event
-             */
-            create: function(id, eventId, callback, error) {
-//                var url = constructUrl(
-//                    this.Constants.routes.users,
-//                    id,
-//                    this.Constants.events,
-//                    eventId
-//                );
-
-//                Restangular.one('api/users', id)
-//                    .one('events', eventId)
-//                    .post()
-//                    .then(callback, error);
-
-//                $http.post(url).then(function(response) {
-//                    console.log(response);
-//                    if (callback) callback(response);
-//                }, function(response) {
-//                    console.log(response);
-//                    if (error) error(response);
-//                });
-            },
-
-
-
-            /**
              * Get creator data
              */
             creatorOf: {
                 index: function(id, callback, error) {
-                    Restangular.one(Constants.routes.users, id)
-                        .one(Constants.events.creatorOf)
-                        .getList()
-                        .then(callback, error);
+                    Users.one(id).one(Constants.events.creatorOf).getList().then(callback, error);
                 },
 
-
                 show: function(id, eventId, callback, error) {
-                    Restangular.one(Constants.routes.users, id)
-                        .one(Constants.events.creatorOf, eventId)
-                        .get()
-                        .then(callback, error);
+                    Users.one(id).one(Constants.events.creatorOf, eventId).get().then(callback, error);
                 }
             },
 
@@ -122,34 +81,19 @@ angular.module('seniorprojectYoApp')
              */
             organizerOf: {
                 index: function(id, callback, error) {
-                    Restangular.one(Constants.routes.users, id)
-                        .one(Constants.events.organizerOf)
-                        .getList()
-                        .then(callback, error);
+                    Users.one(id).one(Constants.events.organizerOf).getList().then(callback, error);
                 },
-
 
                 show: function(id, eventId, callback, error) {
-                    Restangular.one(Constants.routes.users, id)
-                        .one(Constants.events.organizerOf, eventId)
-                        .get()
-                        .then(callback, error);
+                    Users.one(id).one(Constants.events.organizerOf, eventId).get().then(callback, error);
                 },
-
 
                 create: function(id, eventId, callback, error) {
-                    Restangular.one(Constants.routes.users, id)
-                        .one(Constants.events.organizerOf, eventId)
-                        .post()
-                        .then(callback, error);
+                    Users.one(id).one(Constants.events.organizerOf, eventId).post().then(callback, error);
                 },
 
-
                 destroy: function(id, eventId, callback, error) {
-                    Restangular.one(Constants.routes.users, id)
-                        .one(Constants.events.organizerOf, eventId)
-                        .remove()
-                        .then(callback, error);
+                    Users.one(id).one(Constants.events.organizerOf, eventId).remove().then(callback, error);
                 }
             },
 
@@ -160,34 +104,19 @@ angular.module('seniorprojectYoApp')
              */
             volunteeredTo: {
                 index: function(id, callback, error) {
-                    Restangular.one(Constants.routes.users, id)
-                        .one(Constants.events.volunteeredTo)
-                        .getList()
-                        .then(callback, error);
+                    Users.one(id).one(Constants.events.volunteeredTo).getList().then(callback, error);
                 },
-
 
                 show: function(id, eventId, callback, error) {
-                    Restangular.one(Constants.routes.users, id)
-                        .one(Constants.events.volunteeredTo, eventId)
-                        .get()
-                        .then(callback, error);
+                    Users.one(id).one(Constants.events.volunteeredTo, eventId).get().then(callback, error);
                 },
-
 
                 create: function(id, eventId, callback, error) {
-                    Restangular.one(Constants.routes.users, id)
-                        .one(Constants.events.volunteeredTo, eventId)
-                        .post()
-                        .then(callback, error);
+                    Users.one(id).one(Constants.events.volunteeredTo, eventId).post().then(callback, error);
                 },
 
-
                 destroy: function(id, eventId, callback, error) {
-                    Restangular.one(Constants.routes.users, id)
-                        .one(Constants.events.volunteeredTo, eventId)
-                        .remove()
-                        .then(callback, error);
+                    Users.one(id).one(Constants.events.volunteeredTo, eventId).remove().then(callback, error);
                 }
             }
         };
@@ -199,44 +128,17 @@ angular.module('seniorprojectYoApp')
          */
         this.groups = {
 
-            /**
-             * Create a group
-             */
-            create: function(id, params, callback, error) {
-//                var url = constructUrl(
-//                    this.Constants.routes.users,
-//                    id,
-//                    this.Constants.groups
-//                );
-//
-//                $http.post(url, params).then(function(response) {
-//                    console.log(response);
-//                    if (callback) callback(response);
-//                }, function(response) {
-//                    console.log(response);
-//                    if (error) error(response);
-//                });
-            },
-
-
 
             /**
              * Get creator data
              */
             creatorOf: {
                 index: function(id, callback, error) {
-                    Restangular.one(Constants.routes.users, id)
-                        .one(Constants.groups.creatorOf)
-                        .getList()
-                        .then(callback, error);
+                    Users.one(id).one(Constants.groups.creatorOf).getList().then(callback, error);
                 },
 
-
                 show: function(id, groupId, callback, error) {
-                    Restangular.one(Constants.routes.users, id)
-                        .one(Constants.groups.creatorOf, groupId)
-                        .get()
-                        .then(callback, error);
+                    Users.one(id).one(Constants.groups.creatorOf, groupId).get().then(callback, error);
                 }
             },
 
@@ -247,34 +149,19 @@ angular.module('seniorprojectYoApp')
              */
             organizerOf: {
                 index: function(id, callback, error) {
-                    Restangular.one(Constants.routes.users, id)
-                        .one(Constants.groups.organizerOf)
-                        .getList()
-                        .then(callback, error);
+                    Users.one(id).one(Constants.groups.organizerOf).getList().then(callback, error);
                 },
-
 
                 show: function(id, groupId, callback, error) {
-                    Restangular.one(Constants.routes.users, id)
-                        .one(Constants.groups.organizerOf, groupId)
-                        .get()
-                        .then(callback, error);
+                    Users.one(id).one(Constants.groups.organizerOf, groupId).get().then(callback, error);
                 },
-
 
                 create: function(id, groupId, callback, error) {
-                    Restangular.one(Constants.routes.users, id)
-                        .one(Constants.groups.organizerOf, groupId)
-                        .post()
-                        .then(callback, error);
+                    Users.one(id).one(Constants.groups.organizerOf, groupId).post().then(callback, error);
                 },
 
-
                 destroy: function(id, groupId, callback, error) {
-                    Restangular.one(Constants.routes.users, id)
-                        .one(Constants.groups.organizerOf, groupId)
-                        .remove()
-                        .then(callback, error);
+                    Users.one(id).one(Constants.groups.organizerOf, groupId).remove().then(callback, error);
                 }
             },
 
@@ -285,34 +172,19 @@ angular.module('seniorprojectYoApp')
              */
             volunteeredTo: {
                 index: function(id, callback, error) {
-                    Restangular.one(Constants.routes.users, id)
-                        .one(Constants.groups.volunteeredTo)
-                        .getList()
-                        .then(callback, error);
+                    Users.one(id).one(Constants.groups.volunteeredTo).getList().then(callback, error);
                 },
-
 
                 show: function(id, groupId, callback, error) {
-                    Restangular.one(Constants.routes.users, id)
-                        .one(Constants.groups.volunteeredTo, groupId)
-                        .get()
-                        .then(callback, error);
+                    Users.one(id).one(Constants.groups.volunteeredTo, groupId).get().then(callback, error);
                 },
-
 
                 create: function(id, groupId, callback, error) {
-                    Restangular.one(Constants.routes.users, id)
-                        .one(Constants.groups.volunteeredTo, groupId)
-                        .post()
-                        .then(callback, error);
+                    Users.one(id).one(Constants.groups.volunteeredTo, groupId).post().then(callback, error);
                 },
 
-
                 destroy: function(id, groupId, callback, error) {
-                    Restangular.one(Constants.routes.users, id)
-                        .one(Constants.groups.volunteeredTo, groupId)
-                        .remove()
-                        .then(callback, error);
+                    Users.one(id).one(Constants.groups.volunteeredTo, groupId).remove().then(callback, error);
                 }
             }
         };
